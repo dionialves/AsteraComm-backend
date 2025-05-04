03558ba1e1a0e5fc21bb612c07ea6678776eb853
Dioni Alves <dionialves@gmail.com>
2025-05-03 22:46:05 -0300

    Adjust project name

---
+++ src/main/java/com/dionialves/AsteraComm/AsteraCommApplication.java
@@ -0,0 +1,19 @@
+package com.dionialves.AsteraComm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class AsteraCommApplication {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(AsteraCommApplication.class, args);
+    }
+}
--- src/main/java/com/dionialves/AsteraComm/controller/EndPointController.java
+++ src/main/java/com/dionialves/AsteraComm/controller/EndPointController.java
@@ -3,12 +3,14 @@ package com.dionialves.AsteraComm.controller;
 import com.dionialves.AsteraComm.dto.EndPointDTO;
 import com.dionialves.AsteraComm.repository.EndPointRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/circuits")
 public class EndPointController {
